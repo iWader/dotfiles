78eb083b058de48d3e4d081d2eac44ef86822fe2
Wade Urry <wade@iwader.co.uk>
2023-06-19 17:01:28 +0100

    Update nvm

--- nvm/install.sh
+++ nvm/install.sh
@@ -1,1 +1,1 @@
-curl --silent -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | PROFILE="${DOTFILES}/nvm/nvm.zsh" bash
+curl --silent -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | PROFILE="${DOTFILES}/nvm/nvm.zsh" bash
