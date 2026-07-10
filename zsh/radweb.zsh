1a94392543fe265062585350f8168966bea380ac
Wade Urry <wade@iwader.co.uk>
2026-07-10 10:11:33 +0100

    sailcl alias

--- zsh/radweb.zsh
+++ zsh/radweb.zsh
@@ -2,3 +2,5 @@
 alias gup='git fetch origin && grb origin/$(git_current_branch)' # gup instead of gpl
 alias grb='git rebase --rebase-merges'
 
+# Changelog
+alias sailcl='sail artisan make:changelog -C -B $(git branch --show-current)'
